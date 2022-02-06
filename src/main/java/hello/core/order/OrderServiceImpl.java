ca79105903674b6cd371be2a6427df369342f91e
YUNMI LEE <goya3014@hanmail.net>
2022-02-06 11:24:25 +0900

    컴포넌트 스캔

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,7 +4,10 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository; //= new MemoryMemberRepository();
@@ -13,6 +16,7 @@ public class OrderServiceImpl implements OrderService{
     //현재는 인터페이스 뿐만 아니라 구현체에도 의존하고 있으므로 DIP 위반. 따라서 인터페이스만 의존하도록 아래와 같이 코드 변경.
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
