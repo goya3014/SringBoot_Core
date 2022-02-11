910e473e6f0b2cf7f7cf20cd2458e8280dbd2dca
YUNMI LEE <goya3014@hanmail.net>
2022-02-11 15:02:43 +0900

    웹 스코프와 프록시

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
+    //프록시 사용
     private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
@@ -20,6 +23,7 @@ public class LogDemoController {
     public String logDemo(HttpServletRequest request){
 
         String requestURL = request.getRequestURI().toString();
+        //MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
